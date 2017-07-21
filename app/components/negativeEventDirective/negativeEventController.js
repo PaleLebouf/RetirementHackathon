fc9be23d71f376ca97b2b340bd47dea4a086de15
Collin O'Neill <Collin_ONeill@aol.com>
2017-07-21 04:37:19 -0400

    debt paid page added

--- app/components/negativeEventDirective/negativeEventController.js
+++ app/components/negativeEventDirective/negativeEventController.js
@@ -40,6 +40,25 @@
             
         };
 
+        $scope.showDebtPaidOff = function(ev) {
+        // Appending dialog to document.body to cover sidenav in docs app
+            $mdDialog.show({
+            controller: negativeEventController,
+            templateUrl: 'components/negativeEventDirective/debtPaidTemplate.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose:false,
+            fullscreen: false // Only for -xs, -sm breakpoints.
+            })
+            .then(function(answer) {
+            $scope.status = 'You said the information was "' + answer + '".';
+            }, function() {
+            $scope.status = 'You cancelled the dialog.';
+            });
+            
+        };
+        
+
         $scope.cancel = function() {
                 $mdDialog.cancel();
         };
