313421fce952e81d90c4f7be9377c4a745a30e61
David Isovitsch <isovitd@nationwide.com>
2017-07-21 04:26:00 -0400

    Add stub for score calculation

--- app/Services/calculationService.js
+++ app/Services/calculationService.js
@@ -143,5 +143,14 @@ angular.module("retirementRoad").service('calculationService', ["$interval", fun
 	    return months;
     }
 
+    /*
+    * Calculate the player's score.
+    */
+    self.calculateScore = function () {
+        var score = 100;
+
+        return score;
+    }
+
     return self;
 }]);
