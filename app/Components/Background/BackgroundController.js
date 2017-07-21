2ba82fe61e132e5e1ecab3334f9a3f0460a12d35
Trever Smith <smithtr9889@gmail.com>
2017-07-21 05:50:44 -0400

    Backgrounds change now

--- app/Components/Background/BackgroundController.js
+++ app/Components/Background/BackgroundController.js
@@ -1,18 +1,37 @@
 (function() {
 
-    function backgroundController($scope, $interval)
+    function backgroundController($scope, $interval, calculationService)
     {
+        var checkStage = function(){
+            if(calculationService.data.stage == 1)
+            {
+                $scope.staticBackground = "assets/parallaxBackgrounds/roadParallaxBG1-Cut.png";
+                $scope.parallaxBackground = "assets/parallaxBackgrounds/roadParallax1-Cut.png";
+            }
+            else if(calculationService.data.stage == 2)
+            {
+                $scope.staticBackground = "assets/parallaxBackgrounds/roadParallaxBG2-Cut.png";
+                $scope.parallaxBackground = "assets/parallaxBackgrounds/roadParallax2-Cut.png";
+            }
+            else
+            {
+                $scope.staticBackground = "assets/parallaxBackgrounds/roadParallaxBG3-Cut.png";
+                $scope.parallaxBackground = "assets/parallaxBackgrounds/roadParallax3-Cut.png";
+            }
+        };
+
         var swapImage = function() {
             
             if($scope.image == "one")
                 $scope.image = "two";
             else if ($scope.image == "two")
                 $scope.image = "one";
-        }
+        };
 
         $scope.image = "one";
 
         $interval(swapImage, 2000);
+        $interval(checkStage, 50);
     }
 
     angular.module("retirementRoad").controller("backgroundController", backgroundController);
