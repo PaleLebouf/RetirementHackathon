ff7e084ac780af376a6d137b79082a1a11551f1a
Trever Smith <smithtr9889@gmail.com>
2017-07-21 01:38:49 -0400

    negative update

--- app/components/negativeEventDirective/negativeEventController.js
+++ app/components/negativeEventDirective/negativeEventController.js
@@ -7,19 +7,21 @@
 
         $scope.showJobLoss = function(ev) {
         // Appending dialog to document.body to cover sidenav in docs app
-            var confirm = $mdDialog.confirm()
-                .title('You\'re Fired!')
-                .textContent('You lost your job, you\'ll be living of your savings for a while.')
-                .ariaLabel('Lucky day')
-                .targetEvent(ev)
-                .ok('Sweet')
-                .cancel('Just What I Wanted');
-
-            $mdDialog.show(confirm).then(function() {
-            $scope.status = 'You decided to get rid of your debt.';
+            $mdDialog.show({
+            controller: DialogController,
+            templateUrl: 'components/negativeEventDirective/negativeEventTemplate.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose:true,
+            fullscreen: false // Only for -xs, -sm breakpoints.
+            })
+            .then(function(answer) {
+            $scope.status = 'You said the information was "' + answer + '".';
             }, function() {
-            $scope.status = 'You decided to keep your debt.';
+            $scope.status = 'You cancelled the dialog.';
             });
+
+            
         };
     };
 
