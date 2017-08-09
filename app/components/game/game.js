d20c6265496bda6e57f1642dc0cb5d73eab9d149
cblackw <blackwc@bgsu.edu>
2017-08-09 10:59:57 -0400

    Fixing the code I broke in my sloppy check in

--- app/components/game/game.js
+++ app/components/game/game.js
@@ -1,11 +1,12 @@
 (function () {
     function gameCtrl($scope, calcService, eventService) {
         calcService.startGame();
+        $scope.data = calcService.data;
 
         $scope.$watch("data.debt", function(newValue, oldValue, scope) {
             if (newValue <= 0) {
-                eventService.showDebtPaidOff()
-                $scope.data.stage++;
+                eventService.showDebtPaidOff();
+                calcService.data.stage++;
             }
         });
     }
